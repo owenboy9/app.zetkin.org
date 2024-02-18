d866da3c4c4f5e45071d079243839cf3ebd127b0
Richard Olsson <r@richardolsson.se>
2024-02-18 18:15:59 +0100

    Remove slash from apiFetch

--- src/utils/apiFetch.ts
+++ src/utils/apiFetch.ts
@@ -5,11 +5,11 @@ export type ApiFetch = (path: string, init?: RequestInit) => Promise<Response>;
 
 export const createApiFetch = (
   headers: IncomingHttpHeaders,
-  prefix = 'api'
+  prefix = '/api'
 ): ApiFetch => {
   return (path, init) => {
     const protocol = process.env.ZETKIN_APP_PROTOCOL || 'http';
-    const apiUrl = `${protocol}://${process.env.ZETKIN_APP_HOST}/${prefix}${path}`;
+    const apiUrl = `${protocol}://${process.env.ZETKIN_APP_HOST}${prefix}${path}`;
     console.log('apiUrl', apiUrl);
     return fetch(apiUrl, {
       ...init,
